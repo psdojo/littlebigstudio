9ce91d00e03bfc6ec43b436b25e614d2934394bb
psdojo <71039532+psdojo@users.noreply.github.com>
2021-09-12 07:50:41 +0530

    Delete src directory

--- src/assets/js/main.js
+++
@@ -1,5 +0,0 @@
-import '/src/assets/stylesheets/css/tailwind.css';
-import '/src/assets/stylesheets/scss/main.scss';
-import './functions';
-
-
--- src/components/header.js
+++
@@ -1,5 +0,0 @@
-class Header extends HTMLElement { constructor() { super(); }
-connectedCallback() { this.innerHTML = `
-   
-      
-      `; } } customElements.define('header-component', Header);
